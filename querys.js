ff2164a73e59a2de25bc6aeb87839787fa7dbe4d
Luis Miguel <lmmolinabe92@gmail.com>
2024-03-21 09:21:09 +0100

    cargar tabla

--- querys.js
+++ querys.js
@@ -9,4 +9,26 @@ $(document).ready(function () {
             alert("error");
         });
 
+
+    $.get("http://localhost/api/paises/")
+        .done(function (resp) {
+            for (const pais of resp) {
+                let html = "<tr>" +
+                    "<td>"+pais.Name+" </td>" +
+                    "<td>"+pais.Capital+" </td>" +
+                    "<td>"+pais.Continent+"</td>" +
+                    "<td>"+pais.Population+"</td>" +
+                    "<td>"+pais.GNP+"</td>" +
+                    "<td>"+pais.HeadOfState+"</td>" +
+                    "<td>" +
+                    '<a href="?mod=625"><i class="bi bi-pencil-square"></i> Modificar</a> |' +
+                    '<a href="?del=625"><i class="bi bi-trash"></i> Eliminar</a>' +
+                    "</td>" +
+                    "</tr>";
+                $(".tabla_paises tbody").append(html);
+            }
+        })
+        .fail(function () {
+            alert("error");
+        });
 });

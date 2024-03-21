8b9f630c1d99663b42dd82b05c5407f43e63f2d0
Luis Miguel <lmmolinabe92@gmail.com>
2024-03-21 12:23:44 +0100

    fin de paginacion

--- querys.js
+++ querys.js
@@ -1,3 +1,5 @@
+var paises = [];
+var continente_select = "";
 $(document).ready(function () {
     $.get("http://localhost/api/continentes/")
         .done(function (resp) {
@@ -12,8 +14,9 @@ $(document).ready(function () {
 
     $.get("http://localhost/api/paises/")
         .done(function (respP) {
+            paises = respP;
             for (let i = 0; i < respP.length && i < 25; i++) {
-                const pais = respP[i];
+                const pais = paises[i];
                 if (pais.Capital) {
                     $.get("http://localhost/api/ciudades/?id=" + pais.Capital)
                         .done(function (respC) {
@@ -36,7 +39,7 @@ $(document).ready(function () {
             }
             let paginas = Math.ceil(respP.length / 25);
             for (let i = 1; i <= paginas; i++) {
-                $(".paginacion").append("<a href='#'>" + i + "</a>");
+                $(".paginacion").append("<a href='#' onclick='navegarPaginacion("+i+")'>" + i + "</a>");
             }
 
         })
@@ -44,3 +47,70 @@ $(document).ready(function () {
             alert("error");
         });
 });
+
+
+
+function filtrarContinente() {
+    continente_select = $("#continente select").val();
+    $(".tabla_paises tbody").html("");
+    let cont = 0;
+    for (let i = 0; i < paises.length; i++) {
+        const pais = paises[i];
+        if (pais.Continent == continente_select) {
+            cont++;
+            if (cont < 25) {
+                let html = "<tr>" +
+                    "<td>" + pais.Name + " </td>" +
+                    "<td>" + pais.Capital + " </td>" +
+                    "<td>" + pais.Continent + "</td>" +
+                    "<td>" + pais.Population + "</td>" +
+                    "<td>" + pais.GNP + "</td>" +
+                    "<td>" + pais.HeadOfState + "</td>" +
+                    "<td>" +
+                    '<a href="?mod=625"><i class="bi bi-pencil-square"></i> Modificar</a> |' +
+                    '<a href="?del=625"><i class="bi bi-trash"></i> Eliminar</a>' +
+                    "</td>" +
+                    "</tr>";
+                $(".tabla_paises tbody").append(html);
+            }
+        }
+    }
+    let paginas = Math.ceil(cont / 25);
+    $(".paginacion").html("");
+    for (let i = 1; i <= paginas; i++) {
+        $(".paginacion").append("<a href='#' onclick='navegarPaginacion("+i+")'>" + i + "</a>");
+    }
+}
+
+function navegarPaginacion(pag) {
+    $(".tabla_paises tbody").html("");
+    let ini = 25 * (pag - 1);
+    let cont = 0;
+    for (let i = 0; i < paises.length; i++) {
+        const pais = paises[i];
+        if (continente_select != "") {
+            if (pais.Continent == continente_select) {
+                cont++;
+            } else {
+                continue;
+            }
+        } else {
+            cont++;
+        }
+        if (cont >= ini && cont < ini + 25) {
+            let html = "<tr>" +
+                "<td>" + pais.Name + " </td>" +
+                "<td>" + pais.Capital + " </td>" +
+                "<td>" + pais.Continent + "</td>" +
+                "<td>" + pais.Population + "</td>" +
+                "<td>" + pais.GNP + "</td>" +
+                "<td>" + pais.HeadOfState + "</td>" +
+                "<td>" +
+                '<a href="?mod=625"><i class="bi bi-pencil-square"></i> Modificar</a> |' +
+                '<a href="?del=625"><i class="bi bi-trash"></i> Eliminar</a>' +
+                "</td>" +
+                "</tr>";
+            $(".tabla_paises tbody").append(html);
+        }
+    }
+}

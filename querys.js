d5327c474c7561f5149460bd2c83829ff269eaaf
Luis Miguel <lmmolinabe92@gmail.com>
2024-03-21 10:53:32 +0100

    paginacion

--- querys.js
+++ querys.js
@@ -14,24 +14,31 @@ $(document).ready(function () {
         .done(function (respP) {
             for (let i = 0; i < respP.length && i < 25; i++) {
                 const pais = respP[i];
-                $.get("http://localhost/api/ciudades/?id=" + pais.Capital)
-                    .done(function (respC) {
-                        pais.Capital = respC[0].Name;
-                        let html = "<tr>" +
-                            "<td>" + pais.Name + " </td>" +
-                            "<td>" + pais.Capital + " </td>" +
-                            "<td>" + pais.Continent + "</td>" +
-                            "<td>" + pais.Population + "</td>" +
-                            "<td>" + pais.GNP + "</td>" +
-                            "<td>" + pais.HeadOfState + "</td>" +
-                            "<td>" +
-                            '<a href="?mod=625"><i class="bi bi-pencil-square"></i> Modificar</a> |' +
-                            '<a href="?del=625"><i class="bi bi-trash"></i> Eliminar</a>' +
-                            "</td>" +
-                            "</tr>";
-                        $(".tabla_paises tbody").append(html);
-                    });
+                if (pais.Capital) {
+                    $.get("http://localhost/api/ciudades/?id=" + pais.Capital)
+                        .done(function (respC) {
+                            pais.Capital = respC[0].Name;
+                            let html = "<tr>" +
+                                "<td>" + pais.Name + " </td>" +
+                                "<td>" + pais.Capital + " </td>" +
+                                "<td>" + pais.Continent + "</td>" +
+                                "<td>" + pais.Population + "</td>" +
+                                "<td>" + pais.GNP + "</td>" +
+                                "<td>" + pais.HeadOfState + "</td>" +
+                                "<td>" +
+                                '<a href="?mod=625"><i class="bi bi-pencil-square"></i> Modificar</a> |' +
+                                '<a href="?del=625"><i class="bi bi-trash"></i> Eliminar</a>' +
+                                "</td>" +
+                                "</tr>";
+                            $(".tabla_paises tbody").append(html);
+                        });
+                }
             }
+            let paginas = Math.ceil(respP.length / 25);
+            for (let i = 1; i <= paginas; i++) {
+                $(".paginacion").append("<a href='#'>" + i + "</a>");
+            }
+
         })
         .fail(function () {
             alert("error");

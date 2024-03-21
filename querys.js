f7d1d6b3e7779c80c7c9b349cb08cfa2518faa40
Luis Miguel <lmmolinabe92@gmail.com>
2024-03-21 10:37:11 +0100

    tabla

--- querys.js
+++ querys.js
@@ -11,21 +11,26 @@ $(document).ready(function () {
 
 
     $.get("http://localhost/api/paises/")
-        .done(function (resp) {
-            for (const pais of resp) {
-                let html = "<tr>" +
-                    "<td>"+pais.Name+" </td>" +
-                    "<td>"+pais.Capital+" </td>" +
-                    "<td>"+pais.Continent+"</td>" +
-                    "<td>"+pais.Population+"</td>" +
-                    "<td>"+pais.GNP+"</td>" +
-                    "<td>"+pais.HeadOfState+"</td>" +
-                    "<td>" +
-                    '<a href="?mod=625"><i class="bi bi-pencil-square"></i> Modificar</a> |' +
-                    '<a href="?del=625"><i class="bi bi-trash"></i> Eliminar</a>' +
-                    "</td>" +
-                    "</tr>";
-                $(".tabla_paises tbody").append(html);
+        .done(function (respP) {
+            for (let i = 0; i < respP.length && i < 25; i++) {
+                const pais = respP[i];
+                $.get("http://localhost/api/ciudades/?id=" + pais.Capital)
+                    .done(function (respC) {
+                        pais.Capital = respC[0].Name;
+                        let html = "<tr>" +
+                            "<td>" + pais.Name + " </td>" +
+                            "<td>" + pais.Capital + " </td>" +
+                            "<td>" + pais.Continent + "</td>" +
+                            "<td>" + pais.Population + "</td>" +
+                            "<td>" + pais.GNP + "</td>" +
+                            "<td>" + pais.HeadOfState + "</td>" +
+                            "<td>" +
+                            '<a href="?mod=625"><i class="bi bi-pencil-square"></i> Modificar</a> |' +
+                            '<a href="?del=625"><i class="bi bi-trash"></i> Eliminar</a>' +
+                            "</td>" +
+                            "</tr>";
+                        $(".tabla_paises tbody").append(html);
+                    });
             }
         })
         .fail(function () {
